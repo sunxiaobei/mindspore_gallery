eef81c9fcc13573c7f260b55c9f4081930ebf1f8
SunXiaobei <1371815174@qq.com>
2023-12-26 23:29:49 +0800

    调参

--- train_grl_gcn.py
+++ train_grl_gcn.py
@@ -26,7 +26,7 @@ parser.add_argument('--epochs', type=int, default=200, help='Number of epochs to
 parser.add_argument('--lr', type=float, default=0.01, help='Initial learning rate.')
 parser.add_argument('--weight_decay', type=float, default=5e-4, help='Weight decay (L2 loss on parameters).')
 parser.add_argument('--hidden', type=int, default=16, help='Number of hidden units.')
-parser.add_argument('--dropout', type=float, default=0.5, help='Dropout rate (1 - keep probability).')
+parser.add_argument('--dropout', type=float, default=0.2, help='Dropout rate (1 - keep probability).')
 
 args = parser.parse_args()
 np.random.seed(args.seed)  # 设置np.random
